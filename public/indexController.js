591da766a18202eedab093ddbf86fa9476f9c92d
cherryBomba58 <cherryBomba58@users.noreply.github.com>
2016-10-06 16:43:12 +0200

    Webshop.home is linked on the home side, not webshop

--- public/indexController.js
+++ public/indexController.js
@@ -1,5 +1,5 @@
 ﻿angular.module("WebshopEngineApp", ["ui.router"])
-	.config(function($stateProvider) {
+	.config(function($stateProvider, $urlRouterProvider) {
 		$stateProvider
 			.state({
 				name: 'root',
@@ -14,87 +14,73 @@
 			})
 			.state({
 				name: 'webshops.home',
-				url: '',
+				url: '/home',
 				templateUrl: 'webshop/pages/home.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.notfound',
 				url: '/notfound',
 				templateUrl: 'webshop/pages/404.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.about',
 				url: '/about',
 				templateUrl: 'webshop/pages/about.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.contact',
 				url: '/contact',
 				templateUrl: 'webshop/pages/contact.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.faq',
 				url: '/faq',
 				templateUrl: 'webshop/pages/faq.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.fullwidth',
 				url: '/fullwidth',
 				templateUrl: 'webshop/pages/full-width.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.login',
 				url: '/login',
 				templateUrl: 'webshop/pages/login.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.portfolio1',
 				url: '/portfolio1',
 				templateUrl: 'webshop/pages/portfolio-1-col.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.portfolio2',
 				url: '/portfolio2',
 				templateUrl: 'webshop/pages/portfolio-2-col.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.portfolio3',
 				url: '/portfolio3',
 				templateUrl: 'webshop/pages/portfolio-3-col.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.portfolio4',
 				url: '/portfolio4',
 				templateUrl: 'webshop/pages/portfolio-4-col.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.item',
 				url: '/item',
 				templateUrl: 'webshop/pages/portfolio-item.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.regist',
 				url: '/regist',
 				templateUrl: 'webshop/pages/regist.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'webshops.sidebar',
 				url: '/sidebar',
 				templateUrl: 'webshop/pages/sidebar.html',
-				//controller: 'WebshopCtrl'
 			})
 			.state({
 				name: 'admin',
@@ -125,6 +111,6 @@
 			});
 
 	})
-	.controller("WebshopCtrl", function($scope, $stateParams, $state) {
+	.controller("WebshopCtrl", function($scope, $stateParams) {
 		$scope.webshopID = $stateParams.webshopID;
 	});
