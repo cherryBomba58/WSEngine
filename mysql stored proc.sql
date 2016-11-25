7294741e68fe09a075edaed806a5fc5a426c1403
cherryBomba58 <cherryBomba58@users.noreply.github.com>
2016-11-25 14:04:25 +0100

    Bugfix: orders only one order in a cursor iteration

--- mysql stored proc.sql
+++ mysql stored proc.sql
@@ -1,17 +1,17 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `order_cart`(IN userID INT)
 BEGIN
   DECLARE done INTEGER DEFAULT FALSE;
-  DECLARE product, bought_quantity, all_quantity INT(11);
+  DECLARE product, bought_quantity, all_quantity, buyID INT(11);
   DECLARE webshop VARCHAR(50);
   DECLARE invalid_number CONDITION FOR SQLSTATE '22011';
   DECLARE bought_quan_too_big CONDITION FOR SQLSTATE '22012';
-  DECLARE cur CURSOR FOR SELECT productID, webshopID, quantity FROM buy WHERE statusID = 1 AND buyerID = userID;
+  DECLARE cur CURSOR FOR SELECT productID, webshopID, quantity, transID FROM buy WHERE statusID = 1 AND buyerID = userID;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
   OPEN cur;
 
   read_loop: LOOP
-    FETCH cur INTO product, webshop, bought_quantity;
+    FETCH cur INTO product, webshop, bought_quantity, buyID;
     IF done THEN
       LEAVE read_loop;
     END IF;
@@ -20,12 +20,12 @@ BEGIN
     
     IF bought_quantity > all_quantity THEN
       SIGNAL bought_quan_too_big;
-	ELSEIF bought_quantity < 1 THEN
-	  SIGNAL invalid_number;
-	ELSE
-	  UPDATE sells SET quantity = all_quantity - bought_quantity WHERE productID = product AND webshopID = webshop;
-      UPDATE buy SET statusID = 2 WHERE statusID = 1 AND buyerID = userID;
-	END IF;
+    ELSEIF bought_quantity < 1 THEN
+      SIGNAL invalid_number;
+    ELSE
+      UPDATE sells SET quantity = all_quantity - bought_quantity WHERE productID = product AND webshopID = webshop;
+      UPDATE buy SET statusID = 2 WHERE transID = buyID;
+    END IF;
   END LOOP;
 
   CLOSE cur;
